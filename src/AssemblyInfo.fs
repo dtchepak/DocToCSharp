8034b4d584c0d47342df4fbf69db932372b1803f
David Tchepak <dave@davesquared.net>
2014-02-28 22:32:42 +1100

    Added AssemblyInfo to version output EXE

---
+++ src/AssemblyInfo.fs
@@ -0,0 +1,9 @@
+﻿namespace DocToCSharp
+
+open System.Reflection;
+ 
+[<assembly: AssemblyTitle("DocToCSharp")>]
+[<assembly: AssemblyDescription("Extracts code from NSubstitute docs and puts it into a compilable C# project")>]
+[<assembly: AssemblyVersion("0.0.1.0")>]
+[<assembly: AssemblyFileVersion("0.0.1.0")>]
+()
